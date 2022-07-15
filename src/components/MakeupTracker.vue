eefff9d29e3910536cc3add6dded2474caa8c743
themmixproject <steven.devalk@gmail.com>
2022-07-15 23:48:45 +0200

    Fixed styling and added progress bar indicator styling

--- src/components/MakeupTracker.vue
+++ src/components/MakeupTracker.vue
@@ -38,36 +38,46 @@ export default {
         border-radius: 50%;
         flex-shrink: 0;
         align-self: center;
-        .content-container {
-            padding-left: 1em;
-            flex-grow: 1;
-            white-space: nowrap;
-            font-family: Verdana, Geneva, Tahoma, sans-serif;
+    }
+    .content-container {
+        padding-left: 1em;
+        flex-grow: 1;
+        white-space: nowrap;
+        text-align: initial;
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
 
-            display: flex;
-            flex-direction: column;
-            justify-content: space-evenly;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+
+        overflow: hidden;
+        text-overflow: ellipsis;
+
+        .tracker-title {
+            font-size: 1.5em;
+            font-weight: normal;
 
             overflow: hidden;
             text-overflow: ellipsis;
-
-            .tracker-title {
-                font-size: 1.5em;
-                font-weight: normal;
-
-                overflow: hidden;
-                text-overflow: ellipsis;
-            }
-            .tracker-progress-bar {
-                background-color: #d66161;
-                height: 15px;
-
-                border-radius: 20px;
-            }
         }
-        .content-container > * {
-            margin: 2px;
+        .tracker-progress-bar {
+            background-color: #d66161;
+            height: 15px;
+
+            border-radius: 20px;
         }
+
+        .tracker-progress-bar-indicator {
+            background-color: black;
+            height: inherit;
+
+            width: 25%;
+
+            border-radius: 100px;
+        }
+    }
+    .content-container > * {
+        margin: 2px;
     }
 }
 </style>
