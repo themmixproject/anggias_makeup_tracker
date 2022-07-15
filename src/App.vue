c0ef26fe4abaa67dfc736e93b598b9bea84bea9f
themmixproject <steven.devalk@gmail.com>
2022-07-15 23:51:30 +0200

    Deleted boilerplace components from App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
     <MakeupTracker title="Lorem Ipsum" />
+    <MakeupTracker title="Title 2" />
+    <MakeupTracker title="Title 3" />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 import MakeupTracker from "./components/MakeupTracker.vue";
 
 export default {
     name: "App",
     components: {
-        HelloWorld,
         MakeupTracker
     }
 };
