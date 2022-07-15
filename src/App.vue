965285b34cb0c133653ca5a5b7b4d2e5a97ebb99
themmixproject <steven.devalk@gmail.com>
2022-07-15 23:59:28 +0200

    Add AddTrackerButton component to App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,24 @@
     <MakeupTracker title="Lorem Ipsum" />
     <MakeupTracker title="Title 2" />
     <MakeupTracker title="Title 3" />
+    <AddTrackerButton />
 </template>
 
 <script>
 import MakeupTracker from "./components/MakeupTracker.vue";
+import AddTrackerButton from "./components/AddTrackerButton.vue";
 
 export default {
     name: "App",
     components: {
-        MakeupTracker
+        MakeupTracker,
+        AddTrackerButton
     }
 };
 </script>
 
 <style lang="scss">
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+body {
+    margin: 0;
 }
 </style>
