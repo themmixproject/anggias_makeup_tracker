6a92c290cba813d3aa6fc24b36650104dac30f90
themmixproject <steven.devalk@gmail.com>
2022-07-17 12:35:28 +0200

    Updates deploy.sh script with new url and proper branch reference

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ git init
 git add -A
 git commit -m "deploy"
 
-git push -f git@github.com:themmixproject/anggias_makeup_tracker.git master:gh-pages
+git push -f https://github.com/themmixproject/anggias_makeup_tracker.git master:gh-pages
