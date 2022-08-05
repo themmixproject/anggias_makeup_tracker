5bfc65cf255d6ea24755d9ed517e4dff53d75fa1
themmixproject <steven.devalk@gmail.com>
2022-08-05 22:39:25 +0200

    Added BottomMenuButton component

---
+++ src/components/BottomMenuButton.vue
@@ -0,0 +1,20 @@
+<template>
+    <button class="menu-button"></button>
+</template>
+
+<style lang="scss" scoped>
+.menu-button {
+    height: 60px;
+    width: 60px;
+
+    outline: none;
+    border-radius: 50%;
+    border: 0;
+
+    line-height: 60px;
+    color: white;
+    text-align: center;
+
+    box-shadow: 1px 1px 10px 0px #00000082;
+}
+</style>
