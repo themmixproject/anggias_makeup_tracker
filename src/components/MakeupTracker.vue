246dbe9f2e95acf07c09710bac04eb206280e5b6
themmixproject <steven.devalk@gmail.com>
2023-04-15 19:14:01 +0200

    Added meatball menu to MakeupTracker component

--- src/components/MakeupTracker.vue
+++ src/components/MakeupTracker.vue
@@ -1,23 +1,39 @@
 <template>
-    <div class="tracker-item">
-        <div class="icon-placeholder">
-            <inline-svg :src="icon" class="icon-vector"></inline-svg>
+    <div class="tracker-list-item">
+        <div class="meatball-menu-wrapper">
+            <div
+                class="meatball-menu-icon-container"
+                @click="toggleMeatballMenu"
+            >
+                <div class="meatball-menu-icon"></div>
+                <div class="meatball-menu-icon"></div>
+                <div class="meatball-menu-icon"></div>
+            </div>
+            <div class="meatball-menu-container" v-show="meatballMenuDisplay">
+                <div class="meatball-menu-item">delete</div>
+                <div class="meatball-menu-item">edit</div>
+            </div>
         </div>
-        <div class="content-container">
-            <div class="tracker-title">{{ title }}</div>
-            <div class="tracker-date-wrapper">
-                Opened Date:
-                <span class="tracker-date">{{ startDate }}</span>
+        <div class="tracker-list-item-content-container">
+            <div class="icon-container">
+                <inline-svg :src="icon" class="icon-vector"></inline-svg>
             </div>
-            <div class="tracker-date-wrapper">
-                Due Date:
-                <span class="tracker-date">{{ enddate }}</span>
-            </div>
-            <div class="tracker-progress-bar">
-                <div
-                    class="tracker-progress-bar-indicator"
-                    :style="{ width: progressBarWidth }"
-                ></div>
+            <div class="tracker-list-item-attribute-container">
+                <div class="tracker-title">{{ title }}</div>
+                <div class="tracker-date-wrapper">
+                    Opened Date:
+                    <span class="tracker-date">{{ startDate }}</span>
+                </div>
+                <div class="tracker-date-wrapper">
+                    Due Date:
+                    <span class="tracker-date">{{ enddate }}</span>
+                </div>
+                <div class="tracker-progress-bar">
+                    <div
+                        class="tracker-progress-bar-indicator"
+                        :style="{ width: progressBarWidth }"
+                    ></div>
+                </div>
             </div>
         </div>
     </div>
@@ -26,10 +42,11 @@
 <script>
 export default {
     props: ["title", "startdate", "enddate", "icon"],
-    date() {
+    data() {
         return {
             startDate: this.$props.startdate,
-            endDate: this.$props.enddate
+            endDate: this.$props.enddate,
+            meatballMenuDisplay: false
         };
     },
     computed: {
@@ -67,25 +84,63 @@ export default {
             let diffStartEnd = endTime - startTime;
 
             return diffStartEnd / (1000 * 3600 * 24);
+        },
+        toggleMeatballMenu() {
+            this.meatballMenuDisplay = !this.meatballMenuDisplay;
         }
     }
 };
 </script>
 
 <style lang="scss" scoped>
-.tracker-item-container {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-}
-.tracker-item {
+.tracker-list-item {
     margin: 10px;
     border-radius: 20px;
     background-color: lightcoral;
     padding: 0.5em;
-    display: flex;
+    padding-right: 0.8em;
+}
+.meatball-menu-wrapper {
+    position: relative;
 
-    .icon-placeholder {
+    .meatball-menu-icon-container {
+        display: flex;
+        flex-direction: row;
+        position: absolute;
+        // float: right;
+        top: 0px;
+        right: 0px;
+        .meatball-menu-icon {
+            height: 8px;
+            width: 8px;
+            margin: 0 1.5px;
+            border-radius: 50%;
+            background-color: black;
+        }
+    }
+
+    .meatball-menu-container {
+        position: absolute;
+        right: 0px;
+        top: 12px;
+        background-color: rgb(228, 228, 228);
+        border-radius: 10px;
+
+        .meatball-menu-item {
+            text-align: center;
+            padding: 10px 20px;
+            user-select: none;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+        }
+    }
+}
+
+.tracker-list-item-content-container {
+    display: flex;
+    flex-direction: row;
+
+    .icon-container {
         height: 50px;
         width: 50px;
         background-color: #d66161;
@@ -100,7 +155,7 @@ export default {
             width: inherit;
         }
     }
-    .content-container {
+    .tracker-list-item-attribute-container {
         padding-left: 0.5em;
         flex-grow: 1;
         // white-space: nowrap;
@@ -121,6 +176,8 @@ export default {
             white-space: nowrap;
             overflow: hidden;
             text-overflow: ellipsis;
+
+            padding-right: 33px;
         }
 
         .tracker-date {
@@ -139,9 +196,6 @@ export default {
             height: inherit;
             border-radius: 100px;
         }
-    }
-    .content-container > * {
-        margin: 2px;
     }
 }
 </style>
